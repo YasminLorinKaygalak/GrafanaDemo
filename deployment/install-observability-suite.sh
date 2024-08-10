fa6f905d6c68caf7e7c4d441205c41dc19823340
Melisa Griffin <melisa.griffin@hashicorp.com>
2024-08-09 21:06:15 -0400

    Some updates

--- deployment/install-observability-suite.sh
+++ deployment/install-observability-suite.sh
@@ -5,7 +5,7 @@ helm install --values helm/prometheus.yaml prometheus prometheus-community/prome
 kubectl rollout status deployment prometheus-server --namespace default --timeout=300s && \
 helm install loki --values helm/loki.yaml grafana/loki-stack --version "2.9.9" && \
 kubectl rollout status statefulset loki --namespace default --timeout=300s && \
-helm install --values helm/grafana.yaml grafana grafana/grafana --version "6.23.1" && \
+helm upgrade --install --values helm/grafana.yaml grafana grafana/grafana --version "6.23.1" && \
 kubectl rollout status deployment grafana --namespace default --timeout=300s && \
 echo "#######################################" && \
 echo "Observability Suite Deployment Complete" && \
